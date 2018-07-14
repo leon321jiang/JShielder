aebd43465b16ed46c939fbecc2d5c9f2f7314899
leon321jiang <38706185+leon321jiang@users.noreply.github.com>
2018-07-14 01:11:25 -0700

    Update hardening.sh

--- UbuntuServer_16.04LTS/hardening.sh
+++ UbuntuServer_16.04LTS/hardening.sh
@@ -688,8 +688,8 @@ cat <<EOT3 >> /etc/profile
 function log2syslog
 {
    declare COMMAND
-   COMMAND=$(fc -ln -0)
-   logger -p local1.notice -t bash -i -- "${USER}:${COMMAND}"
+   COMMAND=\$(fc -ln -0)
+   logger -p local1.notice -t "bashcommand" -i -- "\${USER}:\${COMMAND}"
 }
 trap log2syslog DEBUG
 EOT3
